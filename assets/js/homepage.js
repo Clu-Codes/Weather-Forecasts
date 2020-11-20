4c9c2b1de47a0dfb34fc0ecf431dedb5f7d70325
Clu <retsulcconnor@gmail.com>
2020-11-20 00:16:54 -0500

    Finished Forecasting. localStorage began.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,22 @@ var cityName = [];
 inputEl = document.querySelector("#city-finder");
 formEl = document.querySelector("form");
 
+var saveCities = function() {
+    localStorage.setItem("localStorageCity", JSON.stringify(cityName));
+};
+
+var loadCities = function () {
+    if (JSON.parse(localStorage.getItem("localStorageCity")) !== null) {
+        cityName = JSON.parse(localStorage.getItem("localStorageCity"));
+    }
+
+    cityName.forEach((city, index) => {
+        citiesLineEl = document.createElement("<li>");
+        cityNameNode = document.createTextNode("index")
+        citiesLineEl.appendChild(cityNameNode);
+        document.getElementById("#search-history").appendChild(citiesLineEl);
+    })
+}
 
 var getCity = function(event) {
     event.preventDefault()
@@ -20,7 +36,7 @@ var getForecast = function(city) {
     .then(function(response) {
         if (response.ok) {
             cityName.push(city);
-            console.log(cityName);
+            saveCities();
             response.json().then(function(data) {
                 var oneDayApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.city.coord.lat + "&lon=" + data.city.coord.lon + "";
                 fetch (oneDayApiUrl)
@@ -52,16 +68,50 @@ var displayCityWeather = function(data) {
     console.log(humidityEl);
 
     windSpeedEl = document.querySelector("#wind-speed");
-    windSpeedEl.textContent = "wind:" + " " + data.current.wind_speed;
+    windSpeedEl.textContent = "Wind-Speeds:" + " " + data.current.wind_speed;
     console.log(windSpeedEl);
 
     uvIndexEl = document.querySelector("#uv-index");
     uvIndexEl.textContent = "UV-Index" + " " + data.current.uvi;
     console.log(uvIndexEl);
 
+    forecastOneDate = document.querySelector("#first-day-header");
+    forecastOneDate.textContent = moment().add(1, 'd').format("MM/DD/YYYY");
+    forecastOneTemp = document.querySelector("#first-day-temp");
+    forecastOneTemp.textContent = "Temp:" + " " + data.daily[1].feels_like.day;
+    forecastOneHumid = document.querySelector("#first-day-humid");
+    forecastOneHumid.textContent = "Humidity:" + " " + data.daily[1].humidity;
+
+    forecastTwoDate = document.querySelector("#second-day-header");
+    forecastTwoDate.textContent = moment().add(2, 'd').format("MM/DD/YYYY");
+    forecastTwoTemp = document.querySelector("#second-day-temp");
+    forecastTwoTemp.textContent = "Temp:" + " " + data.daily[2].feels_like.day;
+    forecastTwoHumid = document.querySelector("#second-day-humid");
+    forecastTwoHumid.textContent = "Humidity:" + " " + data.daily[2].humidity;
+
+    forecastThreeDate = document.querySelector("#third-day-header");
+    forecastThreeDate.textContent = moment().add(3, 'd').format("MM/DD/YYYY");
+    forecastThreeTemp = document.querySelector("#third-day-temp");
+    forecastThreeTemp.textContent = "Temp:" + " " + data.daily[3].feels_like.day;
+    forecastThreeHumid = document.querySelector("#third-day-humid");
+    forecastThreeHumid.textContent = "Humidity:" + " " + data.daily[3].humidity;
+
+    forecastFourDate = document.querySelector("#fourth-day-header");
+    forecastFourDate.textContent = moment().add(4, 'd').format("MM/DD/YYYY");
+    forecastFourTemp = document.querySelector("#fourth-day-temp");
+    forecastFourTemp.textContent = "Temp:" + " " + data.daily[4].feels_like.day;
+    forecastFourHumid = document.querySelector("#fourth-day-humid");
+    forecastFourHumid.textContent = "Humidity:" + " " + data.daily[4].humidity;
+
+    forecastFiveDate = document.querySelector("#fifth-day-header");
+    forecastFiveDate.textContent = moment().add(5, 'd').format("MM/DD/YYYY");
+    forecastFiveTemp = document.querySelector("#fifth-day-temp");
+    forecastFiveTemp.textContent = "Temp:" + " " + data.daily[5].feels_like.day;
+    forecastFiveHumid = document.querySelector("#fifth-day-humid");
+    forecastFiveHumid.textContent = "Humidity:" + " " + data.daily[5].humidity;
+    
 }
 
-
-
+loadCities();
 
 formEl.addEventListener("submit", getCity);
